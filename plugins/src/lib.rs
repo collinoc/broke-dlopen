5cd375936a31612120541a5e78021a0970323619
Collin O'Connor <collinoc+dev@protonmail.com>
2024-04-25 20:46:34 -0500

    Add inline explanations

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -3,13 +3,13 @@ use std::cell::RefCell;
 #[no_mangle]
 pub fn init_func(plugins: &mut Vec<fn()>) {
     plugins.push(do_something);
-    println!("Loaded plugin B");
+    println!("Loaded plugin A");
 }
 
 pub fn do_something() {
     thread_local! {
-        // static CELL: RefCell<u8> = RefCell::new(1);
-        static CELL: RefCell<Box<u8>> = RefCell::new(Box::new(1));
+        // static CELL: RefCell<u8> = RefCell::new(1); // This is fine
+        static CELL: RefCell<Box<u8>> = RefCell::new(Box::new(1)); // This doesn't reload as expected
     }
 
     CELL.with_borrow(|cell| {
