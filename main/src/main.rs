59516785169daa63bd653d0e09fa0b57ed399500
Collin O'Connor <collinoc+dev@protonmail.com>
2024-05-21 21:19:17 -0500

    Simplify

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,28 +1,14 @@
+use std::{thread, time::Duration};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut buffer = String::new();
-
-    let mut libs = Vec::new();
-    let mut plugins = Vec::new();
-
     loop {
-        plugins.clear();
-        libs.clear();
-
-        println!("Press enter to reload...");
-        std::io::stdin().read_line(&mut buffer)?;
-
         unsafe {
-            // This typically would loop over a directory, attempting to load all *.so files
-            // that match a certain signature, but this is just an example
             let lib = libloading::Library::new("../target/release/libplugins.so")?;
-            let plugin: libloading::Symbol<fn(&mut Vec<fn()>)> = lib.get(b"init_func")?;
+            let lib_func: libloading::Symbol<fn()> = lib.get(b"lib_func")?;
 
-            plugin(&mut plugins);
-            libs.push(lib);
+            lib_func();
         }
 
-        for plugin in &plugins {
-            plugin();
-        }
+        thread::sleep(Duration::from_secs(1));
     }
 }
--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -1,15 +1,10 @@
 use std::cell::RefCell;
 
 #[no_mangle]
-pub fn init_func(plugins: &mut Vec<fn()>) {
-    plugins.push(do_something);
-    println!("Loaded plugin A");
-}
-
-pub fn do_something() {
+pub fn lib_func() {
     thread_local! {
         // static CELL: RefCell<u8> = RefCell::new(1); // This is fine
-        static CELL: RefCell<Box<u8>> = RefCell::new(Box::new(1)); // This doesn't reload as expected
+        static CELL: RefCell<Box<u8>> = RefCell::new(Box::new(1)); // This doesn't change if reloaded
     }
 
     CELL.with_borrow(|cell| {
